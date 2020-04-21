2b8234e15986e31f939b753e9743f8ba18f632b6
Anuhya Vyasam <anuhya.30490@gmail.com>
2020-04-21 09:02:10 -0400

    responsive UI changes

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // db: 'mongodb://localhost:27017/coffee-store'
-    db: 'mongodb+srv://admin:admin@cluster0-nekzi.mongodb.net/coffee-store'
+    db: 'mongodb://localhost:27017/coffee-store'
+    // db: 'mongodb+srv://admin:admin@cluster0-nekzi.mongodb.net/coffee-store'
   };
--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./story.component.css']
 })
 export class StoryComponent implements OnInit {
-  story = "We make coffee! More specifically, we’ve done it for over 20 years. It’s exceptional tasting coffee. And that could be why we’re America’s #1 selling Whole Bean coffee. But it could also be because all our coffee is Organic. And all our coffee is Fairtrade. And it’s all deep and delicious, roasted right in the Canadian Rocky Mountains. It could also be because our customers are loyal, fervent and demand goodness in every cup. And it’s not just our coffee that’s good. We were named the #1 Best Workplace in USA by Great Place to Work® in 2020. We were named a 2019 Fairtrade favourite product. We know how to work hard and have fun. And we think we’re just getting started. We believe great coffee is the answer to many things.";
+ 
   constructor() { }
 
   ngOnInit(): void {
