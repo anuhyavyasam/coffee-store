290e86afdc351d0d1acbf5ebfdc2a34f659838a8
Anuhya Vyasam <anuhya.30490@gmail.com>
2020-04-27 20:21:25 -0400

    cart notification defect - fix the count

--- src/app/cart-notification/cart-notification.component.ts
+++ src/app/cart-notification/cart-notification.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { MessengerService } from '../service/messenger.service';
 import { Subscription } from 'rxjs';
+import { Product } from '../models/product';
 
 @Component({
     selector: 'app-cart-notification',
@@ -9,25 +10,30 @@ import { Subscription } from 'rxjs';
     styleUrls: ['./cart-notification.component.css']
   })
   export class CartNotificationComponent implements OnInit {
-    productCount;
+    productCount = 0;
     messages: any[] = [];
     subscription: Subscription;
     constructor(private productService: ProductService,
         private messageService: MessengerService) {
-        const products = productService.getCheckedOutProducts();
-        this.productCount = products.length;
+        const products  = this.productService.getCheckedOutProducts();
+        for(let product of products) {
+          this.productCount += product.quantity;
+        }
 
         this.subscription = this.messageService.getMessage().subscribe(message => {
             if (message) {
+              this.productCount ++;
               this.messages.push(message);
             } else {
               // clear messages when empty message received
+              this.productCount = 0;
               this.messages = [];
             }
           });
      }
   
     ngOnInit(): void {
+
     }
 
   }
