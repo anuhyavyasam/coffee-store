f0eb4a6cf79b2a40d5bcb1b8486f700d46c8f925
Anuhya Vyasam <anuhya.30490@gmail.com>
2020-04-27 20:23:43 -0400

    whole bean component -msg notification

--- src/app/whole-bean-menu/whole-bean-item/whole-bean-item.component.ts
+++ src/app/whole-bean-menu/whole-bean-item/whole-bean-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from 'src/app/models/product';
 
 import { ProductService } from 'src/app/service/product.service';
+import { MessengerService } from 'src/app/service/messenger.service';
 
 @Component({
   selector: 'app-whole-bean-item',
@@ -11,11 +12,13 @@ import { ProductService } from 'src/app/service/product.service';
 export class WholeBeanItemComponent implements OnInit {
   @Input() productItem: Product;
   @Output() productSelected = new EventEmitter();
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private messageService: MessengerService) { }
 
   ngOnInit(): void {
   }
   handleAddtoCart() {
+    this.messageService.sendMessage(this.productItem);
     this.productService.checkedOutProducts(this.productItem);
   }
 
